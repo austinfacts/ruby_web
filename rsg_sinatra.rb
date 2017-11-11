30c16a7da00dc98893cb170e90cabeeb4d8ba53e
Gabriel A Contreras <gabrielc1@mail.usf.edu>
2017-11-11 12:57:23 -0500

    Adding the required files

--- rsg_sinatra.rb
+++ rsg_sinatra.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 # require 'sinatra/reloader' if development?
 require './rsg'
 
+get '/' do
+  'Hello work!'
+end
 
 get '/:filename' do
   input = params[:filename]
   if input.match?(/[^\w\.\-]/)
-    "That is not right"
+    'That is not right'
   else
     rsg(params[:filename])
   end

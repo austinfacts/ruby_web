445f146857b70193d8d08b84d7f96d92a2553741
Gabriel A Contreras <gabrielc1@mail.usf.edu>
2017-11-11 15:24:01 -0500

    Changes

--- rsg_sinatra.rb
+++ rsg_sinatra.rb
@@ -10,11 +10,11 @@ get '/test' do
   "why you no work?"
 end
 
-get '/Star-Trek-episode' do
-  # input = params[:filename]
-  # if input.match?(/[^\w\.\-]/)
-  #   'That is not right'
-  # else
-    rsg('Star-Trek-episode')
-  # end
+get '/:filename' do
+  input = params[:filename]
+  if input.match?(/[^\w\.\-]/)
+    'That is not right'
+  else
+    rsg(params[:filename])
+  end
 end

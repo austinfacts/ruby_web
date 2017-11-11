72a99bd5779e3d663b19d81d66171fd923d512d1
Austin Lackey <lackeyaustin@gmail.com>
2017-11-11 16:41:01 -0500

    Making the UI work

--- rsg_sinatra.rb
+++ rsg_sinatra.rb
@@ -1,20 +1,36 @@
 require 'sinatra'
-# require 'sinatra/reloader' if development?
+require 'sinatra/reloader' if development?
 require './rsg'
+require 'erb'
 
-get '/' do
-  @title = "Hello work!"
+# get '/' do
+#   @title = "Hello work!"
+# end
+
+
+get '/index' do
+  filename = params['filename']
+  unless filename.nil?
+    if filename.match?(/[^\w\.\-]/)
+      'That is not right'
+    else
+      @data = rsg(filename)
+      erb :ui
+    end
+  else
+    erb :ui
+  end
 end
 
 get '/test' do
   "why you no work?"
 end
 
-get '/:filename' do
-  input = params[:filename]
-  if input.match?(/[^\w\.\-]/)
-    'That is not right'
-  else
-    rsg(params[:filename])
-  end
-end
+# get '/:filename' do
+#   input = params[:filename]
+#   if input.match?(/[^\w\.\-]/)
+#     'That is not right'
+#   else
+#     rsg(params[:filename])
+#   end
+# end
